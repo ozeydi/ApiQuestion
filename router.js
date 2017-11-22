dd87c46d722efe57609265f3110ba269a16e3aa8
Zeydi <oussema.zeidi@ieee.org>
2017-11-22 11:01:38 +0100

    added answers routers

--- router.js
+++ router.js
@@ -18,3 +18,28 @@ router.get('/:qID', (req, res)=> {
     response: `looking at special answer id: ${req.params.qID}`
   });
 });
+
+router.post('/:qID/answers', (req, res) => {
+  res.json({
+    response: 'creating answers',
+    question: req.params.qID,
+    body: req.body
+  });
+});
+
+router.put('/:qID/answers/:aID', (req, res) => {
+  res.json({
+    response: 'editing answers',
+    question: req.params.qID,
+    answer: req.params.aID,
+    body: req.body
+  });
+});
+router.delete('/:qID/answers/:aID', (req, res)=> {
+  res.json({
+    response: 'deleting answers',
+    question: req.params.qID,
+    answer: req.params.aID,
+    body: req.body
+  });
+});
